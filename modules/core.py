d8089a941cb1598113dace4bb46f880e1a9529df
Spacerat <spacerat3004@gmail.com>
2010-08-03 00:54:27 +0100

    !reload.

--- modules/core.py
+++ modules/core.py
@@ -3,7 +3,7 @@ import modules
 import command
 
 def command_load(interface,command,args):
-    """!load modname - Load a module."""
+    """~load modname - Load a module."""
     try:
         modules.add_module(args)
         interface.reply("Loaded %s"%args)
@@ -13,9 +13,14 @@ def command_load(interface,command,args):
         interface.reply(str(e))
 
 def command_unload(interface,command,args):
-    """!unload modname - Unload a module."""
+    """~unload modname - Unload a module."""
     modules.unload_module(args)
     interface.reply("Unloaded %s"%args)
 
+def command_reload(interface,command,args):
+    command_unload(interface,command,args)
+    command_load(interface,command,args)
+
 command.ComHook('load',command_load,security=4,name='ChatBot')
 command.ComHook('unload',command_unload,security=4,name='ChatBot')
+command.ComHook('reload',command_reload,security=4,name='ChatBot')
